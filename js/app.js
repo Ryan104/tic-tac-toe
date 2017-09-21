359cd6b6bdcd711dd7bc0af6dde19ab3a75683f9
Ryan <ryan.d.elliott@gmail.com>
2017-09-20 21:23:37 -0600

    initialize gameState variable

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	console.log('Document is ready');
 
 	let moveCount = 0; // count turn number
+	let boardDimension = 3;
+
+	gameState = initialGameState(boardDimension);
 
 	// --- Table Cell Click Listener --- //
 	//   players move is marked on gameboard and cell is removed from play
@@ -35,7 +38,29 @@ $(document).ready(function(){
 		$('.cell').click(markCell);
 	}
 
+
+	// --- Check for a win condition --- //
+
+	/*
+		Keep track of game state in a 2d matrix/array:
+		[[cell1, cell2, cell3], [cell4, cell5, cell6], [cell7, cell8, cell9]]
+		each cell can have 3 states, empty, "x", or "o"
+
+	*/
+
 });
+
+function initialGameState(dimension){
+	// dimension is the size of the board ie 3 => 3x3
+	let arr = [];
+	for (let i=0; i < dimension; i++){
+		arr.push([]);
+		for (let j=0; j < dimension; j++){
+			arr[i].push("");
+		}
+	}
+	return arr;
+}
 
 
 
