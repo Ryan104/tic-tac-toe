cb2610f9600f4b8296b3a32b749cc2391bc24ef9
Ryan <ryan.d.elliott@gmail.com>
2017-09-20 21:01:02 -0600

    Reset button clears board and resets move count

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
 $(document).ready(function(){
 	console.log('Document is ready');
 
-	// count turn number
-	let moveCount = 0;
+	let moveCount = 0; // count turn number
 
-	// Set the click listener for player moves
+	// --- Table Cell Click Listener --- //
+	//   players move is marked on gameboard and cell is removed from play
 	$('.cell').click(markCell);
 
-
-	// Define the function to be used as a callback in the click listener
 	function markCell(event){
-		console.log(this);
+		// remove the click listener for this element
+		$(this).off();
+
+		// mark board with cat or pig depending on turn
 		$(this).addClass((moveCount % 2) ? 'em em-cat' : 'em em-pig');
 
 		// next move
 		$('.player').toggleClass('current-player');
 		moveCount += 1;
-		// remove the click listener so the cell cant be clicked again
-		$(this).off();
+
+		
+		
+	}
+
+
+	// --- Reset Button Click Listener --- //
+	$('.reset-game').click(resetGame);
+
+	function resetGame(event){
+		console.log('reset');
+		moveCount = 0;
+		$('.player').removeClass('current-player');
+		$('#player-x').addClass('current-player');
+		$('.cell').removeClass('em em-cat em-pig');
+		$('.cell').click(markCell);
 	}
 
 });
+
+
+
+// The game can curently be broken by clicking really fast sometimes...
